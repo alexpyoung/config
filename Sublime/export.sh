7642eb5fdd364b2bef06380c0fbe929f78619981
Alex Young <alexyoung1992@gmail.com>
2018-03-03 09:29:16 -0500

    feat(export.sh): implement export script for Sublime settings

---
+++ Sublime/export.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then rootPath=~/Library/Application Support/Sublime Text 3/Packages/User
+else rootPath=$1
+fi
+
+cd rootPath
+cp *.sublime-settings ./
+cp *.sublime-keymap ./
+
+guardMasterBranch() {
+    currentBranch=$(git branch | sed -n '/\* /s///p')
+    if [ "$currentBranch" != "master" ]
+    then
+        echo "$1 is not on master. $1 needs to be on master to run this script."
+        exit 1
+    fi
+}
+
+guardMasterBranch ./
+
+git fetch origin master
+git rebase origin master
+git commit -am "Update Sublime config"
+git push origin master
+
+echo "Sublime config updated."
