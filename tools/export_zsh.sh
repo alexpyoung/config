4b95371fab22f9964b067e559981342ffb46284f
Alex Young <alexyoung1992@gmail.com>
2018-03-04 12:12:40 -0500

    feat(export_zsh): implement script

---
+++ tools/export_zsh.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+clean() {
+    rm ./.zshrc
+}
+
+clone() {
+    if [ $# -eq 0 ]
+    then target_path=$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+    else target_path=$1
+    fi
+    root_path=$(pwd)
+    cp $HOME/.zshrc "$root_path"
+}
+
+commit() {
+    git stash
+    git fetch origin
+    git rebase origin master
+    git stash pop
+    git add .
+    git commit -m "feat(zsh): update config"
+    git push origin master
+}
+
+guard_master() {
+    current_branch=$(git branch | sed -n '/\* /s///p')
+    if [ "$current_branch" != "master" ]
+    then
+        echo "$1 is not on master. $1 needs to be on master to run this script."
+        exit 1
+    fi
+}
+
+guard_master ./
+pwd=$(pwd)
+# Run from root dir
+cd ./zsh
+clean
+clone
+commit
+cd "$pwd"
