9afc915dd8939e7f4e6facc1a2759790550747b3
Alex Young <alexyoung1992@gmail.com>
2018-03-18 21:18:07 -0400

    refactor(export_alfred): use common scripts

--- tools/export_alfred.sh
+++ tools/export_alfred.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 clean() {
     rm -r *.alfredpreferences
@@ -6,39 +6,21 @@ clean() {
 
 clone() {
     if [ $# -eq 0 ]
-    then target_path=$HOME/Library/Application\ Support/Alfred\ 3/
-    else target_path=$1
+    then targetPath=$HOME/Library/Application\ Support/Alfred\ 3/
+    else targetPath=$1
     fi
-    root_path=$(pwd)
-    cd "$target_path"
-    cp -R *.alfredpreferences "$root_path"
-    cd "$root_path"
+    rootPath=$(pwd)
+    cd $targetPath
+    cp -R *.alfredpreferences $rootPath
+    cd $rootPath
 }
 
-guard_master() {
-    current_branch=$(git branch | sed -n '/\* /s///p')
-    if [ "$current_branch" != "master" ]
-    then
-        echo "$1 is not on master. $1 needs to be on master to run this script."
-        exit 1
-    fi
-}
-
-commit() {
-    git stash
-    git fetch origin
-    git rebase origin master
-    git stash pop
-    git add .
-    git commit -m "feat(Alfred): update config"
-    git push origin master
-}
-
-guard_master ./
-pwd=$(pwd)
+source ./tools/git.sh
+guard_master_branch ./
+cwd=$(pwd)
 # Run from root dir
 cd ./Alfred
 clean
 clone
-commit
-cd "$pwd"
+commit 'Alfred'
+cd $cwd
