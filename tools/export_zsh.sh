67fd12063e3da317ece88325e782b3f4b01d2021
Alex Young <alexyoung1992@gmail.com>
2018-03-18 21:18:49 -0400

    refactor(export_zsh): use common scripts

--- tools/export_zsh.sh
+++ tools/export_zsh.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 clean() {
     rm ./.zshrc
@@ -6,33 +6,15 @@ clean() {
 
 clone() {
     root_path=$(pwd)
-    cp $HOME/.zshrc "$root_path"
+    cp $HOME/.zshrc $root_path
 }
 
-commit() {
-    git stash
-    git fetch origin
-    git rebase origin master
-    git stash pop
-    git add .
-    git commit -m "feat(zsh): update config"
-    git push origin master
-}
-
-guard_master() {
-    current_branch=$(git branch | sed -n '/\* /s///p')
-    if [ "$current_branch" != "master" ]
-    then
-        echo "$1 is not on master. $1 needs to be on master to run this script."
-        exit 1
-    fi
-}
-
-guard_master ./
-pwd=$(pwd)
+source ./tools/git.sh
+guard_master_branch ./
+cwd=$(pwd)
 # Run from root dir
 cd ./zsh
 clean
 clone
-commit
-cd "$pwd"
+commit 'zsh'
+cd $cwd
