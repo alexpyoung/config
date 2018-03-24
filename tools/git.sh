fa0d245299cb6634dd196b74dcf213000ca3d17e
Alex Young <alexyoung1992@gmail.com>
2018-03-24 13:30:06 -0400

    refactor(git): use local variables

--- tools/git.sh
+++ tools/git.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
 guard_master_branch() {
-    currentBranch=$(git branch | sed -n '/\* /s///p')
-    if [ $currentBranch != 'master' ]
+    local -r WORKING_DIR=$1
+    local -r CURRENT_BRANCH=$(git branch | sed -n '/\* /s///p')
+    if [ $CURRENT_BRANCH != 'master' ]
     then
-        echo "$1 is not on master. $1 needs to be on master to run this script."
+        echo "$WORKING_DIR is not on master. $WORKING_DIR needs to be on master to run this script."
         exit 1
     fi
 }
 
 commit() {
+    local -r GROUP_NAME=$1
     git stash
     git fetch origin
     git rebase origin master
     git stash pop
     git add .
-    git commit -m "feat($1): update config"
+    git commit -m "feat($GROUP_NAME): update config"
     git push origin master
 }
