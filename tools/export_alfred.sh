63d9567391f7cf3150a2eab8092787ed24a85ad1
Alex Young <alexyoung1992@gmail.com>
2018-05-13 14:43:35 -0400

    fix(export_alfred): directory management

--- tools/export_alfred.sh
+++ tools/export_alfred.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env bash
 
 clean() {
-    rm -r *.alfredpreferences
+    local -r DESTINATION=$1
+    rm -r "$DESTINATION"/*.alfredpreferences
 }
 
 clone() {
-    if [ $# -eq 0 ]
-    then targetPath=$HOME/Library/Application\ Support/Alfred\ 3/
-    else targetPath=$1
-    fi
-    rootPath=$(pwd)
-    cd $targetPath
-    cp -R *.alfredpreferences $rootPath
-    cd $rootPath
+    local -r DESTINATION=$1
+    cp -r "$HOME"/Library/Application\ Support/Alfred\ 3/*.alfredpreferences "$DESTINATION"
 }
 
 source ./tools/git.sh
 guard_master_branch ./
-cwd=$(pwd)
-# Run from root dir
-cd ./Alfred
-clean
-clone
+WD=./Alfred
+clean $WD
+clone $WD
 commit 'Alfred'
-cd $cwd
+
