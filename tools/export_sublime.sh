154b966dadfc365a2d98fc8919a91c9be405222b
Alex Young <alexyoung1992@gmail.com>
2018-05-13 18:19:17 -0400

    refactor(export_sublime): enable execute from any directory

--- tools/export_sublime.sh
+++ tools/export_sublime.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
 clean() {
-    local -r DESTINATION=$1
-    pushd "$DESTINATION" || exit 1
     rm ./*.sublime-settings
     rm ./*.sublime-keymap
-    popd || exit 1
 }
 
 clone() {
-    pushd "$1" || exit 1
     local -r DESTINATION=$(pwd)
-    popd || exit 1
-    pushd "$HOME"/Library/Application\ Support/Sublime\ Text\ 3/Packages/User || exit 1
-    cp ./*.sublime-settings "$DESTINATION"
-    cp ./*.sublime-keymap "$DESTINATION"
-    popd || exit 1
+    local -r TARGET="$HOME"/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+    cp "$TARGET"/*.sublime-settings "$DESTINATION"
+    cp "$TARGET"/*.sublime-keymap "$DESTINATION"
 }
 
-source ./tools/git.sh
-guard_master_branch ./
-WD=./Sublime
-clean "$WD"
-clone "$WD"
-commit 'Sublime'
+
+main() {
+    pushd "${0%/*}" || exit 1
+    source ./git.sh
+    guard_master_branch ../
+    pushd ../Sublime || exit 1
+    clean
+    clone
+    commit 'Sublime'
+    popd && popd || exit 1
+}
+
+main
 
