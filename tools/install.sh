e91faec133b45d814e357435fc7ba1fa47bcfb99
Alex Young <alexyoung1992@gmail.com>
2018-05-13 19:11:14 -0400

    fix(install): audit script

--- tools/install.sh
+++ tools/install.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
+install_homebrew() {
+    pushd /usr/local || exit 1
+    mkdir homebrew
+    curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+    popd || exit 1
+}
+
 main() {
-    pushd /usr/local
-    mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
-    popd
-    # Run from config root dir
-    cp ./.* $HOME/
+    install_homebrew
+    pushd "${0%/*}" || exit 1
+    pushd .. || exit 1
+    cp ./.* ~
     brew bundle exec -- bundle install
-    cat ./.crontab | pbcopy
+    pbcopy < crontab
     echo 'crontab has been loaded into clipboard.'
-    cp ./Sublime/* $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-    cp ./Alfred/* $HOME/Library/Application\ Support/Alfred\ 3/
+    popd || exit 1
+    cp ../Sublime/* ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+    cp ../Alfred/* ~/Library/Application\ Support/Alfred\ 3/
     echo 'Install successful!'
-    cd ../
-    rm -rf ./config
+    popd || exit 1
 }
 
 main
