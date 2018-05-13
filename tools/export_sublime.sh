b46f31a55c46405da980070d09aab0fb33ba4a84
Alex Young <alexyoung1992@gmail.com>
2018-05-13 16:53:25 -0400

    fix(export_sublime): shellcheck

--- tools/export_sublime.sh
+++ tools/export_sublime.sh
@@ -3,15 +3,15 @@
 clean() {
     local -r DESTINATION=$1
     pushd "$DESTINATION" || exit 1
-    rm *.sublime-settings
-    rm *.sublime-keymap
-    popd
+    rm ./*.sublime-settings
+    rm ./*.sublime-keymap
+    popd || exit 1
 }
 
 clone() {
     pushd "$1" || exit 1
     local -r DESTINATION=$(pwd)
-    popd
+    popd || exit 1
     pushd "$HOME"/Library/Application\ Support/Sublime\ Text\ 3/Packages/User || exit 1
     cp ./*.sublime-settings "$DESTINATION"
     cp ./*.sublime-keymap "$DESTINATION"
