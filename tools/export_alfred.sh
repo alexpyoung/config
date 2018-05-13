ad235855a4ab16ecfc4663bb08fb4f0bfc0f7761
Alex Young <alexyoung1992@gmail.com>
2018-05-13 16:59:35 -0400

    refactor(export_alfred): enable execute from any directory

--- tools/export_alfred.sh
+++ tools/export_alfred.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 clean() {
-    local -r DESTINATION=$1
-    rm -r "$DESTINATION"/*.alfredpreferences
+    rm -r ./*.alfredpreferences
 }
 
 clone() {
-    local -r DESTINATION=$1
-    cp -r "$HOME"/Library/Application\ Support/Alfred\ 3/*.alfredpreferences "$DESTINATION"
+    cp -r "$HOME"/Library/Application\ Support/Alfred\ 3/*.alfredpreferences ./
 }
 
-source ./tools/git.sh
-guard_master_branch ./
-WD=./Alfred
-clean $WD
-clone $WD
-commit 'Alfred'
+main() {
+    pushd "${0%/*}" || exit 1
+    source ./git.sh
+    guard_master_branch ../
+    pushd ../Alfred || exit 1
+    clean
+    clone
+    commit 'Alfred'
+    popd && popd || exit 1
+}
+
+main
 
