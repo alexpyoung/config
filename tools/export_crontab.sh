373def793d192bd0b62cae9a8f6a6a5f93d1162a
Alex Young <alexyoung1992@gmail.com>
2018-05-13 18:04:05 -0400

    refactor(export_crontab): enable execute from any directory

--- tools/export_crontab.sh
+++ tools/export_crontab.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 main() {
-    # Run from root dir
-    source ./tools/git.sh
+    pushd "${0%/*}" || exit 1    
+    source ./git.sh
+    pushd .. || exit 1
     guard_master_branch ./
     crontab -l > ./.crontab
     commit 'crontab'
+    popd && popd || exit 1
 }
 
 main
