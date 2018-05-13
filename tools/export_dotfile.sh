e77617d3f4d518fa41647959dd7866ceec0fe5c7
Alex Young <alexyoung1992@gmail.com>
2018-05-13 16:51:59 -0400

    refactor(export_dotfile): name variables

--- tools/export_dotfile.sh
+++ tools/export_dotfile.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 clean() {
-    rm "./.$1"
+    local -r FILENAME="$1"
+    rm ./."$FILENAME"
 }
 
 clone() {
-    root_path=$(pwd)
-    cp $HOME/".$1" $root_path
+    cp "$HOME"/."$1" ./
 }
 
 main() {
     source ./tools/git.sh
     guard_master_branch ./
+    local -r FILENAME="$1"
     # Run from root dir
-    clean $1
-    clone $1
-    commit "$1"
+    clean "$FILENAME"
+    clone "$FILENAME"
+    commit "$FILENAME"
 }
 
-main $1
+main "$1"
