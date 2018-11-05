773bfae0e53d95ba70b3c5dc44c2382fa2aa0e34
Alex Young <alexyoung1992@gmail.com>
2018-11-05 14:00:18 -0500

    feat(Brewfile): update config

--- tools/install.sh
+++ tools/install.sh
@@ -39,7 +39,9 @@ install_zsh() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     source ~/.zshrc
     sleep 15
-    cp -R ./plugins ~/.oh-my-zsh/custom/plugins/
+    cp -R ./plugins "$ZSH_CUSTOM/plugins/"
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
     popd || exit 1
 }
 
