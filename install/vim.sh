4ca1d409fce43f1361fbbcf9b7a016e85a963540
Alex Young <ayoung@managedbyq.com>
2019-02-27 14:04:08 -0500

    fix(vim): install vim plug

--- install/vim.sh
+++ install/vim.sh
@@ -2,10 +2,20 @@
 
 set -e
 
-main() {
+install_vim_plug() {
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
+install_dotfiles() {
     pushd "${0%/*}"
-    cp -r ../vim/* ~
+    cp -r ../vim ~
     popd
+}
+
+main() {
+    install_vim_plug
+    install_dotfiles
 }
 
 main
